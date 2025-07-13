3007c76aca7076c2928b942c88003c174741d12c
pranavreddy529 <y.pranav5292015@gmail.com>
2025-07-13 20:08:45 +0530

    third commit

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 4000;
 await connectDB()
 await connectCloudinary()
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-dusky-theta.vercel.app']
 
 //Middleware configuration
 app.use(express.json());
